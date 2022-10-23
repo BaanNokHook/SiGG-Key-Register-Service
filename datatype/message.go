c6d210fcee73ad98f60edd7051d72ce69405bc41
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-10-23 13:58:58 +0700

    Clean-Code-Key-Register-Service-datatype-message

---
+++ datatype/message.go
@@ -0,0 +1,20 @@
+package datatype
+
+//Message implements the sentry interface that a message is in a event request
+
+type Message struct {   
+	Message    *string	`json:"message,omitempty"`  
+	Formatted  *string      `json:"formatted,omitempty"`  
+	Params     *[]string	`json:"params.omitempty"`	
+}  
+package datatype
+
+//Message implements the Key-Register interface that a message is in a event request
+type Message struct {
+	Message    *string	`json:"message,omitempty"`
+	Formatted  *string      `json:"formatted,omitempty"`   
+	Params     *[]string    `json:"params,omitempty"`         
+}  
+
+
+
