6fb3de61e70e373fbfb7dc2c7e2f5ef95681b73f
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-10-24 20:05:45 +0700

    Clean-Code-Key-Register-Service-key-register-test-events_test-go

---
+++ keyregister/test/events_test.go
@@ -0,0 +1,84 @@
+package keyregister
+
+import (
+	"testing"
+
+	"github.com/atlassian/go-keyregister-api/datatype"
+)
+
+func TestEventsResource(t *testing.T) {
+	client := newTestClient(t)
+
+	org, err := client.GetOrganization(getDefaultOrg())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	team, cleanup := createTeamHelper(t)
+	defer cleanup()
+
+	project, cleanupproj := createProjectHelper(t, team)
+	defer cleanupproj()
+
+	createMessagesHelper(t, client, org, project, 10)
+
+	issues, _, err := client.GetIssues(org, project, nil, nil, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(issues) == 0 {
+		t.Fatalf("no issues found for project %s", project.Name)
+	}
+
+	t.Run("Read out all events for a issue", func(t *testing.T) {
+
+		for _, issue := range issues {
+
+			t.Logf("Issue: %s", *issue.ID)
+
+			_, err = client.GetLatestEvent(issue)
+			if err != nil {
+				t.Error(err)
+			}
+
+			_, err = client.GetOldestEvent(issue)
+			if err != nil {
+				t.Error(err)
+			}
+		}
+
+		t.Run("Convert all entries to proper types if we can", func(t *testing.T) {
+
+			oldest, err := client.GetOldestEvent(issue[0])
+			if err != nil {
+				t.Error(err)
+			}
+
+			if len(oldest.Entries) == 0 {
+				t.Fatal("no entries found and is nil")
+			}
+
+			for _, entry := range oldest.Entries {
+				name, inter, err := entry.GetInterface()
+				if err != nil {
+					t.Error(err)
+				}
+				t.Logf("Name: %s, Interface: %v, Error: %v", name, inter, err)
+
+				if name == "message" {
+					if inter == nil {
+						t.Error("iter message is nil")
+					}
+
+					if m := inter.(*datatype.Message); m != nil {
+						t.Logf("Messages: %v", m.Message)
+					} else {
+						t.Errorf("Message is nil %v", m)
+					}
+				}
+			}
+
+		})
+	})
+}
