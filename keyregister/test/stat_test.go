cbad6e7bd2e38296e2c8d1f64b76fdff41b2ae36
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-10-26 16:15:39 +0700

    Clean-Code-Key-Register-Service-key-register-test-stat_test-go

---
+++ keyregister/test/stat_test.go
@@ -0,0 +1,50 @@
+package keyregister
+
+import (
+	"testing"
+	"time"
+)
+
+func TestStat(t *testing.T) {
+	t.Parallel()
+
+	now := time.Now().Unix()
+	hourlater := time.Duration(1) * time.Hour
+	later := now - int64(hourlater.Seconds())
+
+	client := newTestClient(t)
+
+	org, err := client.GetOrganization(getDefaultOrg())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("run Getting Organization Stats", func(t *testing.T) {
+		if err != nil {
+			t.Fatal(err)
+		}
+		stats, err := client.GetOrganizationStats(org, StatReceived, later, now, nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if len(stats) <= 0 {
+			t.Error("No stats were returned")
+		}
+	})
+
+	t.Run("Run Getting Team Stats", func(t *testing.T) {
+		team, cleanup := createTeamHelper(t)
+		defer cleanup()
+
+		_, err = client.GetTeamStats(org, team, StatReceived, later, now, nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = client.DeleteTeam(org, team)
+		if err != nil {
+			t.Error("Failed to delete test team")
+		}
+	})
+}
